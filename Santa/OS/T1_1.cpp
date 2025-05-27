3d38987822d62ad8427f03ae8d12867f18b0e86a
BaiChu0713 <1785762670@qq.com>
2025-05-28 01:57:26 +0800

    关于内核态标志位PSW

---
+++ Santa/OS/T1_1.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+struct STATE
+{
+    short PSW;//状态标志位 0/1=用户态/内核态；中断则引发用户态转向内核态；内核态转用户态则使用特权指令
+
+};
+
+
+using namespace std;
+int main(){
+    printf("Hello world!");
+    return 0;
+}
